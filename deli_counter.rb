0f4404f15c463f70c23dda5bda20ae8ffdcb451d
George  Pianka <gpianka1@gmail.com>
2018-04-21 13:48:52 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,4 +11,5 @@ def line(guests)
 end
 
 def take_a_number(guests, name)
-  puts "Welcome #{name}! You are currently number #{guests.size + 1}" in line
+  puts "Welcome #{name}! You are currently number #{guests.size + 1} in line."
+end
