2598a98d0782d0f72127574043fe887817726be1
George  Pianka <gpianka1@gmail.com>
2018-04-21 13:38:07 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,9 +2,9 @@ def line(guests)
   if guests.length == 0 
     puts "The line is currently empty."
   else
-    string = "The line is currently: "
+    string = "The line is currently:"
     guests.map.with_index do |guest, index| 
-      string << "#{index + 1}. #{guest}"
+      string << " #{index + 1}. #{guest}"
     end
     puts string 
   end
